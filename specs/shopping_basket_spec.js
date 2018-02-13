29dc00bd8daff713f942f67dd14e60a0e2f021cf
Max Robson <maxwell.robson@gmail.com>
2018-02-13 15:59:14 +0000

    Shopping basket can have multiple items

--- specs/shopping_basket_spec.js
+++ specs/shopping_basket_spec.js
@@ -10,6 +10,7 @@ describe('ShoppingBasket', function(){
   beforeEach(function(){
     shopping_basket = new ShoppingBasket();
     item = new Item('Banana', 0.30);
+    item2 = new Item('Jellybeans', 1.00);
   });
 
   it('should start with and empty item array', function (){
@@ -20,5 +21,23 @@ describe('ShoppingBasket', function(){
     shopping_basket.addItem(item);
     const actual = shopping_basket.lengthOfItemArray();
     assert.strictEqual(actual, 1);
+  });
+  it('item has name', function(){
+    const actual = item.name;
+    assert.strictEqual(actual, 'Banana')
+  });
+  it('item has price', function(){
+    const actual = item.price;
+    assert.strictEqual(actual, 0.30);
+  });
+  it('item starts bogof false', function(){
+    const actual = item.bogof;
+    assert.strictEqual(actual, false);
+  });
+  it('can add multiple items to shopping basket', function() {
+    shopping_basket.addItem(item);
+    shopping_basket.addItem(item2);
+    const actual = shopping_basket.lengthOfItemArray();
+    assert.strictEqual(actual, 2);
   });
 });
